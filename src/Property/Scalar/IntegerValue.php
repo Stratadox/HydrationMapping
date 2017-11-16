de9522ad256a3823fc5ea831aa9fb2890e48b502
Stratadox <jesse@stratadox.com>
2017-11-16 23:07:13 +0100

    Implement negative integer support

--- src/Property/Scalar/IntegerValue.php
+++ src/Property/Scalar/IntegerValue.php
@@ -10,10 +10,10 @@ class IntegerValue extends Scalar
     public function value(array $data, $owner = null) : int
     {
         $value = $this->my($data);
-        if (!preg_match('/^[0-9]+$/', (string) $value)) {
+        if (!preg_match('/^[-+]?\d+$/', (string) $value)) {
             throw UnmappableProperty::itMustBeLikeAnInteger($this, $value);
         }
-        if ($value > PHP_INT_MAX) {
+        if ($value > PHP_INT_MAX || $value < PHP_INT_MIN) {
             throw UnmappableProperty::itMustInIntegerRange($this, $value);
         }
         return (int) $value;
