fa98f7da46addb4db8fe495f1d8b3f16dad53871
Stratadox <info@stratadox.com>
2018-04-29 20:20:01 +0200

    Implement nullable type mapping

---
+++ src/Property/Scalar/CanBeNull.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Stratadox\Hydration\Mapping\Property\Scalar;
+
+use function is_null;
+use Stratadox\HydrationMapping\ExposesDataKey;
+use Stratadox\HydrationMapping\MapsProperty;
+
+/**
+ * Decorates scalar type declaration with a nullable property.
+ *
+ * @package Stratadox\Hydrate
+ * @author  Stratadox
+ */
+final class CanBeNull implements MapsProperty
+{
+    private $or;
+
+    private function __construct(ExposesDataKey $mapping)
+    {
+        $this->or = $mapping;
+    }
+
+    /**
+     * Creates a new nullable type wrapper.
+     *
+     * @param ExposesDataKey $mapping    The mapping to decorate.
+     * @return self                      The custom truth boolean mapping.
+     */
+    public static function or(ExposesDataKey $mapping): self
+    {
+        return new self($mapping);
+    }
+
+    /** @inheritdoc */
+    public function value(array $data, $owner = null)
+    {
+        if (is_null($data[$this->or->key()])) {
+            return null;
+        }
+        return $this->or->value($data, $owner);
+    }
+
+    /** @inheritdoc */
+    public function name(): string
+    {
+        return $this->or->name();
+    }
+}
