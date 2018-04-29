f8145f1e8c149f8c1564abfac4fc5fec3ef5c87a
Stratadox <info@stratadox.com>
2018-04-29 20:28:51 +0200

    Implement original value mapping

---
+++ src/Property/Scalar/OriginalValue.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Stratadox\Hydration\Mapping\Property\Scalar;
+
+/**
+ * Maps the value directly onto an object property.
+ *
+ * @package Stratadox\Hydrate
+ * @author  Stratadox
+ */
+final class OriginalValue extends Scalar
+{
+    /** @inheritdoc */
+    public function value(array $data, $owner = null)
+    {
+        return $this->my($data);
+    }
+}
