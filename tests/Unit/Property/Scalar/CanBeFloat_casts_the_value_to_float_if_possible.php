802500116c79cbd9770ac8554bd70c8f8aea7169
Stratadox <info@stratadox.com>
2018-05-03 01:18:51 +0200

    Add test for possibly float mapping

---
+++ tests/Unit/Property/Scalar/CanBeFloat_casts_the_value_to_float_if_possible.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+namespace Stratadox\HydrationMapping\Test\Unit\Property\Scalar;
+
+use PHPUnit\Framework\TestCase;
+use Stratadox\Hydration\Mapping\Property\Scalar\BooleanValue;
+use Stratadox\Hydration\Mapping\Property\Scalar\CanBeFloat;
+use Stratadox\Hydration\Mapping\Property\Scalar\StringValue;
+use Stratadox\HydrationMapping\UnmappableInput;
+
+/**
+ * @covers \Stratadox\Hydration\Mapping\Property\Scalar\CanBeFloat
+ * @covers \Stratadox\Hydration\Mapping\Property\MissingTheKey
+ * @covers \Stratadox\Hydration\Mapping\Property\UnmappableProperty
+ */
+class CanBeFloat_casts_the_value_to_float_if_possible extends TestCase
+{
+    /** @test */
+    function mapping_integer_like_values_to_integers()
+    {
+        $source = ['mixed' => '16'];
+
+        $map = CanBeFloat::or(StringValue::inProperty('mixed'));
+
+        $this->assertSame(16.0, $map->value($source));
+    }
+
+    /** @test */
+    function falling_back_to_the_alternative_if_the_input_is_not_numeric()
+    {
+        $source = ['mixed' => 'NaN'];
+
+        $map = CanBeFloat::or(StringValue::inProperty('mixed'));
+
+        $this->assertSame('NaN', $map->value($source));
+    }
+
+    /** @test */
+    function retrieving_which_property_to_map_to()
+    {
+        $map = CanBeFloat::or(StringValue::inProperty('mixed'));
+
+        $this->assertSame('mixed', $map->name());
+    }
+
+    /** @test */
+    function retrieving_the_key_to_use_for_the_input_data()
+    {
+        $map = CanBeFloat::or(
+            StringValue::inPropertyWithDifferentKey('mixed', 'key')
+        );
+
+        $this->assertSame('key', $map->key());
+    }
+
+    /** @test */
+    function missing_input_throws_an_exception()
+    {
+        $map = CanBeFloat::or(StringValue::inProperty('mixed'));
+
+        $this->expectException(UnmappableInput::class);
+        $this->expectExceptionCode(0);
+        $this->expectExceptionMessage(
+            'Missing the key `mixed` for property `mixed` the ' .
+            'input data: []; Mapper: ' . CanBeFloat::class
+        );
+
+        $map->value([]);
+    }
+
+    /** @test */
+    function adding_to_the_exception_message_of_the_wrapped_mapping()
+    {
+        $source = ['mixed' => 'NaN'];
+
+        $map = CanBeFloat::or(BooleanValue::inProperty('mixed'));
+
+        $this->expectException(UnmappableInput::class);
+        $this->expectExceptionCode(0);
+        $this->expectExceptionMessage(
+            'Cannot assign `NaN` to property `mixed`: it is not clean for ' .
+            'conversion to boolean. It could not be mapped to float either.'
+        );
+
+        $map->value($source);
+    }
+}
