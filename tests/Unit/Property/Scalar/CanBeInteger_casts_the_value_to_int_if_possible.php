cc20f242e9bbe2fcba9022678188ebf6f5c1ac65
Stratadox <info@stratadox.com>
2018-05-03 00:24:08 +0200

    Add tests for possibly integer values

---
+++ tests/Unit/Property/Scalar/CanBeInteger_casts_the_value_to_int_if_possible.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace Stratadox\HydrationMapping\Test\Unit\Property\Scalar;
+
+use PHPUnit\Framework\TestCase;
+use Stratadox\Hydration\Mapping\Property\Scalar\CanBeInteger;
+use Stratadox\Hydration\Mapping\Property\Scalar\FloatValue;
+
+/**
+ * @covers \Stratadox\Hydration\Mapping\Property\Scalar\CanBeInteger
+ */
+class CanBeInteger_casts_the_value_to_int_if_possible extends TestCase
+{
+    /** @test */
+    function mapping_integer_like_values_to_integers()
+    {
+        $source = ['number' => '16'];
+
+        $map = CanBeInteger::or(FloatValue::inProperty('number'));
+
+        $this->assertSame(16, $map->value($source));
+    }
+
+    /** @test */
+    function falling_back_to_the_alternative_if_the_input_is_not_an_integer()
+    {
+        $source = ['number' => '1.6'];
+
+        $map = CanBeInteger::or(FloatValue::inProperty('number'));
+
+        $this->assertSame(1.6, $map->value($source));
+    }
+
+    /** @test */
+    function retrieving_which_property_to_map_to()
+    {
+        $map = CanBeInteger::or(FloatValue::inProperty('number'));
+
+        $this->assertSame('number', $map->name());
+    }
+
+    /** @test */
+    function retrieving_the_key_to_use_for_the_input_data()
+    {
+        $map = CanBeInteger::or(
+            FloatValue::inPropertyWithDifferentKey('bool', 'key')
+        );
+
+        $this->assertSame('key', $map->key());
+    }
+}
