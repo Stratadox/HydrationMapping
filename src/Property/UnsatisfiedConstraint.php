7da6ffee6b6c6c29ad6fdc90e9898839b47ab348
Stratadox <info@stratadox.com>
2018-05-05 18:33:23 +0200

    Use class name in exception message if the input is an object

--- src/Property/UnsatisfiedConstraint.php
+++ src/Property/UnsatisfiedConstraint.php
@@ -3,13 +3,12 @@ declare(strict_types=1);
 
 namespace Stratadox\Hydration\Mapping\Property;
 
-use function gettype;
-use function is_scalar;
+use function get_class;
+use function is_object;
 use function sprintf;
 use InvalidArgumentException as InvalidArgument;
 use Stratadox\HydrationMapping\MapsProperty;
 use Stratadox\HydrationMapping\UnmappableInput;
-use function trim;
 
 /**
  * Notifies the client code when the input is not accepted by the constraint.
@@ -30,6 +29,14 @@ final class UnsatisfiedConstraint extends InvalidArgument implements UnmappableI
         MapsProperty $property,
         $value
     ): self {
+        if (is_object($value)) {
+            return new self(sprintf(
+                'Cannot assign the `%s` to property `%s`: ' .
+                'The value did not satisfy the specifications.',
+                get_class($value),
+                $property->name()
+            ));
+        }
         return new self(sprintf(
             'Cannot assign `%s` to property `%s`: ' .
             'The value did not satisfy the specifications.',
